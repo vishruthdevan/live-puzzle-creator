734a6dc6fdf816c2e50bdf413e21022beaf922da
Vishruth <vishruthdevan@gmail.com>
2022-01-05 19:26:47 +0530

    Create canvas element

--- script.js
+++ script.js
@@ -1,18 +1,24 @@
-let promise = navigator.mediaDevices.getUserMedia({ video: true });
-promise
-  .then(function (signal) {
-    VIDEO = document.createElement("video");
-    VIDEO.srcObject = signal;
-    VIDEO.play();
-
-    VIDEO.onloadeddata = function () {
-      updateCanvas();
-    };
-  })
-  .catch(function (err) {
-    alert("Error accessing camera: " + err);
-  });
+let CANVAS = null;
+let CONTEXT = null;
 
 const main = () => {
-  console.log("main");
+  CANVAS = document.getElementById("cnv");
+  CONTEXT = CANVAS.getContext("2d");
+  CANVAS.width = window.innerWidth;
+  CANVAS.height = window.innerHeight;
+
+  let promise = navigator.mediaDevices.getUserMedia({ video: true });
+  promise
+    .then(function (signal) {
+      VIDEO = document.createElement("video");
+      VIDEO.srcObject = signal;
+      VIDEO.play();
+
+      VIDEO.onloadeddata = function () {
+        updateCanvas();
+      };
+    })
+    .catch(function (err) {
+      alert("Error accessing camera: " + err);
+    });
 };
