af7b55379a53cffba3fb110ec795bbc3461d9831
Vishruth <vishruthdevan@gmail.com>
2022-01-05 19:21:32 +0530

    Create video src from user media

--- script.js
+++ script.js
@@ -1,3 +1,18 @@
+let promise = navigator.mediaDevices.getUserMedia({ video: true });
+promise
+  .then(function (signal) {
+    VIDEO = document.createElement("video");
+    VIDEO.srcObject = signal;
+    VIDEO.play();
+
+    VIDEO.onloadeddata = function () {
+      updateCanvas();
+    };
+  })
+  .catch(function (err) {
+    alert("Error accessing camera: " + err);
+  });
+
 const main = () => {
   console.log("main");
 };
