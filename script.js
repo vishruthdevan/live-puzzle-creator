928105495c77884a9f6f4492f506782fd428261a
Vishruth <vishruthdevan@gmail.com>
2022-01-07 14:41:16 +0530

    make canvas resize based on window size

--- script.js
+++ script.js
@@ -6,8 +6,6 @@ let SIZE = { x: 0, y: 0, width: 0, height: 0 };
 const main = () => {
   CANVAS = document.getElementById("cnv");
   CONTEXT = CANVAS.getContext("2d");
-  CANVAS.width = window.innerWidth;
-  CANVAS.height = window.innerHeight;
 
   let promise = navigator.mediaDevices.getUserMedia({ video: true });
   promise
@@ -17,17 +15,8 @@ const main = () => {
       VIDEO.play();
 
       VIDEO.onloadeddata = function () {
-        let resizer =
-          SCALER *
-          Math.min(
-            window.innerWidth / VIDEO.innerWidth,
-            window.innerHeight / VIDEO.videoHeight
-          );
-        SIZE.width = resizer * VIDEO.videoWidth;
-        SIZE.height = resizer * VIDEO.videoHeight;
-        SIZE.x = window.innerWidth / 2 - SIZE.width / 2;
-        SIZE.y = window.innerHeight / 2 - SIZE.height / 2;
-
+        handleResize();
+        window.addEventListener("resize", handleResize);
         updateCanvas();
       };
     })
@@ -39,4 +28,20 @@ const main = () => {
 function updateCanvas() {
   CONTEXT.drawImage(VIDEO, 0, 0);
   window.requestAnimationFrame(updateCanvas);
+}
+
+function handleResize() {
+  CANVAS.width = window.innerWidth;
+  CANVAS.height = window.innerHeight;
+
+  let resizer =
+    SCALER *
+    Math.min(
+      window.innerWidth / VIDEO.innerWidth,
+      window.innerHeight / VIDEO.videoHeight
+    );
+  SIZE.width = resizer * VIDEO.videoWidth;
+  SIZE.height = resizer * VIDEO.videoHeight;
+  SIZE.x = window.innerWidth / 2 - SIZE.width / 2;
+  SIZE.y = window.innerHeight / 2 - SIZE.height / 2;
 }
