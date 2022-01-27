5f29cccb901dc44f4f9965cad05f544a77c0a416
Vishruth <vishruthdevan@gmail.com>
2022-01-27 23:05:00 +0530

    fix: draw image issue

--- script.js
+++ script.js
@@ -95,9 +95,12 @@ function addEventListeners() {
 }
 
 function updateCanvas() {
-  // CONTEXT.drawImage(VIDEO, SIZE.x, SIZE.y, SIZE.width, SIZE.height);
   // CONTEXT.drawImage(VIDEO, 0, 0);
   CONTEXT.clearRect(0, 0, CANVAS.width, CANVAS.height);
+  CONTEXT.globalAlpha = 0.1;
+  CONTEXT.drawImage(VIDEO, SIZE.x, SIZE.y, SIZE.width, SIZE.height);
+
+  CONTEXT.globalAlpha = 1;
   for (let i = 0; i < PIECES.length; i++) {
     PIECES[i].draw(CONTEXT);
   }
