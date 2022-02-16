7dd094049a0019ef473af0553684cccda626662e
Vishruth <vishruthdevan@gmail.com>
2022-02-16 19:21:28 +0530

    chore: format timer

--- script.js
+++ script.js
@@ -57,8 +57,25 @@ function restart() {
 function updateTime() {
     let now = new Date().getTime();
     if (START_TIME != null) {
-        document.getElementById("time").innerHTML = now - START_TIME;
+        document.getElementById("time").innerHTML = formatTime(
+            now - START_TIME
+        );
     }
+}
+
+function formatTime(milliseconds) {
+    let seconds = Math.floor(milliseconds / 1000);
+    let s = Math.floor(seconds % 60);
+    let m = Math.floor((seconds % (60 * 60)) / 60);
+    let h = Math.floor((seconds % (60 * 60 * 24)) / (60 * 60));
+
+    let formattedTime = h.toString().padStart(2, "0");
+    formattedTime += ":";
+    formattedTime = m.toString().padStart(2, "0");
+    formattedTime += ":";
+    formattedTime = s.toString().padStart(2, "0");
+
+    return formattedTime;
 }
 
 function getPressedPiece(loc) {
